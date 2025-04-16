0773bbb1e2473392924f1e01859ca1ec6612a586
mohammedanaf <mohammedanaf.developer@gmail.com>
2025-04-16 14:55:31 +0530

    Features section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import LogoShowcase from "./components/LogoShowcase"
+import LogoShowcase from "./sections/LogoShowcase"
 import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import Showcase from "./sections/Showcase"
+import FeatureCards from "./sections/FeatureCards"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Showcase />
       <LogoShowcase />
+      <FeatureCards />
     </>
   )
 }
