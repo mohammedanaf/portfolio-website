eb7dd36cdb4a5b983dce3ec1f4983642fc7fec0b
mohammedanaf <mohammedanaf.developer@gmail.com>
2025-04-16 13:55:58 +0530

    Navigation Bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import Showcase from "./sections/Showcase"
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <Showcase />
     </>
