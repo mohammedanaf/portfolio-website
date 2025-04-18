04813362f636649877858cdd5b347ca4c7fd95f5
mohammedanaf <mohammedanaf.developer@gmail.com>
2025-04-19 00:34:21 +0530

    Footer section added

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,29 @@
+import { socialImgs } from "../constants"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="flex flex-col justify-center md:items-start items-center">
+                    <p>Terms & Conditions</p>
+                </div>
+
+                <div className="socials">
+                    {socialImgs.map((img) => (
+                        <a href={img.url} key={img.url} target="_blank" className="icon">
+                            <img src={img.imgPath} alt={img.name} />
+                        </a>
+                    ))}
+                </div>
+
+                <div className="flex flex-col justify-center">
+                    <p className="text-center md:text-end">
+                        &copy; {new Date().getFullYear()} Mohammed Anaf. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
